52f9f62659d5f8cf15fbe5aeaffe0f7c128efb39
Maik Klein <maikklein@googlemail.com>
2017-05-01 13:03:30 +0200

    update

--- config.sh
+++ config.sh
@@ -8,3 +8,10 @@ ln -s -f "$(pwd)/vim/.vimrc" ~/.vimrc
 mkdir -p ~/.i3
 ln -s -f "$(pwd)/i3/.i3/config" ~/.i3/config
 ln -s -f "$(pwd)/X/.Xresources" ~/.Xresources
+ln -s -f "$(pwd)/X/.Xresources" ~/.Xresources
+
+mkdir -p ~/.config/bspwm
+ln -s -f "$(pwd)/bspwm/.config/bspwm/bspwmrc" ~/.config/bspwm/bspwmrc
+mkdir -p ~/.config/sxhkd
+ln -s -f "$(pwd)/bspwm/.config/sxhkd/sxhkdrc" ~/.config/sxhkd/sxhkdrc
+ln -s -f "$(pwd)/compton/compton.conf" ~/compton.conf
