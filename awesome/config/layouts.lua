5f2107cd05a28a4f9ad59562427f4a39d558f196
Maik Klein <maikklein@googlemail.com>
2020-02-26 15:27:45 +0100

    Update

---
+++ awesome/config/layouts.lua
@@ -0,0 +1,26 @@
+local awful = require("awful")
+-- Table of layouts to cover with awful.layout.inc, order matters.
+return {
+    tile_index = 1,
+    max_index = 2,
+    layouts = {
+        awful.layout.suit.tile.left,
+        awful.layout.suit.max,
+        -- awful.layout.suit.tile.left,
+        -- awful.layout.suit.tile.top,
+        -- awful.layout.suit.tile.bottom,
+        -- awful.layout.suit.floating,
+        -- awful.layout.suit.fair,
+        -- awful.layout.suit.fair.horizontal,
+        -- awful.layout.suit.spiral,
+        -- awful.layout.suit.spiral.dwindle,
+        -- awful.layout.suit.max,
+        -- awful.layout.suit.magnifier
+        -- awful.layout.suit.magnifier,
+        -- awful.layout.suit.corner.nw,
+        -- awful.layout.suit.corner.ne,
+        -- awful.layout.suit.corner.sw,
+        -- awful.layout.suit.corner.se,
+    }
+}
+-- }}}
